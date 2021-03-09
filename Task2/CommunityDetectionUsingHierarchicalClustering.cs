e2e64f059e962f025828efcf09b6be75463ca93e
Ondra Szkandera <szkanderao@gmail.com>
2021-03-09 16:23:09 +0100

    master - init Task 2

---
+++ Task2/CommunityDetectionUsingHierarchicalClustering.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task2
+{
+    public class CommunityDetectionUsingHierarchicalClustering
+    {
+        public void DetectComunities()
+        {
+            //TODO: Vymyslet implementaci
+        }
+    }
+}
